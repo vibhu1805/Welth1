fa7387c0c2779b608d50b42f5d1aed6d8e8a4615
VaishnaviMishra <vaishnavi@sekeron.com>
2025-02-03 02:32:52 +0530

    Myapp

--- my-app/components/header.jsx
+++ my-app/components/header.jsx
@@ -8,7 +8,7 @@ import { LayoutDashboard, PenBox } from "lucide-react";
 
 const header = () => {
   return (
-    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+    <div className="fixed top-0 w-full bg-white/100 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
