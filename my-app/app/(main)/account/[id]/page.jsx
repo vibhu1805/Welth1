13656e4032010c701877d492c84edf9f442f37c0
Ishi <ishigupta1201@gmail.com>
2025-02-08 01:44:25 +0530

    Done till monthly  budget start from 4:00 inngest cron job

--- my-app/app/(main)/account/[id]/page.jsx
+++ my-app/app/(main)/account/[id]/page.jsx
@@ -1,9 +1,13 @@
 import { getAccountWithTransactions } from '@/actions/account'
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react'
+import {TransactionTable} from '../_components/transaction-table.jsx';
+import { BarLoader } from 'react-spinners';
+import { AccountChart } from '../_components/account-chart.jsx';
 
 const AccountsPage = async ({params}) => {
-  const accountData = await getAccountWithTransactions(params.id);
+  const { id } = await params; //Ensuring params is awaited // added due to error occuring in params by ISHI, not in OG gitrepo 
+  const accountData = await getAccountWithTransactions(id);
  
   if(!accountData){
     notFound();
@@ -11,31 +15,44 @@ const AccountsPage = async ({params}) => {
   const {transactions, ...account} = accountData;
 
   return (
-    <div>
-      <div>
-        <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
             {account.name}
-        </h1>
-        <p className="text-muted-foreground">
+          </h1>
+          <p className="text-muted-foreground">
             {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
             Account
-        </p>
-      </div>
-      <div className="text-right pb-2">
-        <div className="text-xl sm:text-2xl font-bold">
-          ${parseFloat(account.balance).toFixed(2)}
+          </p>
         </div>
-        <p className="text-sm text-muted-foreground">
-          {account._count.transactions} Transactions
-        </p>
+
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
       </div>
+
+      {/* Chart Section */}
+      
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart/>
+      </Suspense>
+      {/* Transaction Table */}
+
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <TransactionTable />
+      </Suspense>
+
     </div>
-
-
-    //************************************************* */
-    // TRANSACTION SECTION - ISHI YAHAN SE START KARNA
-    //************************************************ */
-    
   )
 }
 
--- my-app/app/(main)/dashboard/page.jsx
+++ my-app/app/(main)/dashboard/page.jsx
@@ -1,22 +1,43 @@
 import { Suspense } from "react";
-
-
-
-
+import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData } from "@/actions/dashboard";
+import { getCurrentBudget } from "@/actions/budget";
+import { AccountCard } from "./_components/account-card";
+import { CreateAccountDrawer } from "@/components/create-account-drawer";
+import { BudgetProgress } from "./_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import { CreateAccountDrawer } from "@/components/create-account-drawer";
-import { getUserAccounts } from "@/actions/dashboard";
-import { AccountCard } from "./_components/account-card";
-
+// import { DashboardOverview } from "./_components/transaction-overview";
 
 export default async function DashboardPage() {
-  const account = await getUserAccounts()
-  
+  const [accounts, transactions] = await Promise.all([
+    getUserAccounts(),
+    getDashboardData(),
+  ]);
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
     <div className="space-y-8">
-      
+      {/* Budget Progress */}
+      <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
+
+      {/* Commented because this file has yet not been created so was giving errors - ISHI */}
+
+      {/* Dashboard Overview */}
+      {/* <DashboardOverview
+        accounts={accounts}
+        transactions={transactions || []}
+      /> */}
 
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -28,8 +49,8 @@ export default async function DashboardPage() {
             </CardContent>
           </Card>
         </CreateAccountDrawer>
-        {account.length > 0 &&
-          account?.map((account) => (
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
             <AccountCard key={account.id} account={account} />
           ))}
       </div>
