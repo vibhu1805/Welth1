9f5240c1b5f7c19a4ed2b9e5113cddb94a4f7d41
Vibhuti Sharma <109684722+vibhu1805@users.noreply.github.com>
2025-09-20 11:49:36 +0530

    Update layout.js

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Copied With Love By V Square</p>
+            <p>Made By Vibhuti</p>
           </div>
         </footer>
 
